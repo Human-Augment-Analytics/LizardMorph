316e10db12170f0e44a1ecf7224e9d8d4ad03555
le yang loh <leyangloh@gmail.com>
2025-07-25 10:20:16 -0400

    backend changes

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -4,7 +4,14 @@ import multiprocessing
 max_requests = 1000
 max_requests_jitter = 50
 
-log_file = "-"
+log_file = "/var/log/lizardmorph"
+
+# Log file configurations
+accesslog = "/var/log/lizardmorph/access.log"
+errorlog = "/var/log/lizardmorph/error.log"
+
+# Daemon configuration
+daemon = True
 
 bind = "0.0.0.0:3000"
 
