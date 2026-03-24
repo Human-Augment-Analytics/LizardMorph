9e85280d40609f7212be7af31bce0cbac07d3b82
le yang loh <leyangloh@gmail.com>
2026-03-24 17:03:21 -0400

    feat: set default scale landmarks to 1 and 2 for all view types

--- frontend/src/views/MainView.tsx
+++ frontend/src/views/MainView.tsx
@@ -93,8 +93,8 @@ export class MainView extends Component<MainProps, MainState> {
     lizardCount: 0,
     zoomTransform: d3.zoomIdentity,
     scaleSettings: {
-      pointAId: this.props.selectedViewType === 'toepads' ? 0 : null,
-      pointBId: this.props.selectedViewType === 'toepads' ? 1 : null,
+      pointAId: 0,
+      pointBId: 1,
       value: 10,
       units: "mm",
     },
